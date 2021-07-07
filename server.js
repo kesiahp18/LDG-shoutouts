de9857b97aa89e97c51fba1df8ef88984dda3758
kesiahp18 <kesiahp18@gmail.com>
2021-07-07 16:08:55 -0400

    create user model and its get/create routes

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+const sess = {
+    secret: 'secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(routes);
 
 sequelize.sync({ force: false, debug: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
